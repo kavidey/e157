f9f1d7d6c79c9e6e5c028cf857d7030fd5aac106
kavidey <kaviasher@gmail.com>
2024-09-24 00:22:03 -0700

    Add data for main part of lab 1

--- scripts/environment.sh
+++ scripts/environment.sh
@@ -1,2 +1,5 @@
-alias ltspice_to_s1p='ltspice_to_s1p.sh';
-export PATH="$PATH:$(dirname "$0")"
+export RF_SCRIPTS_PATH="$(dirname "$0")";
+
+alias ltspice_to_s1p='$RF_SCRIPTS_PATH/ltspice_to_s1p.sh';
+
+alias ltspice_to_s1p_all="find . -name '*.txt' -exec $RF_SCRIPTS_PATH/ltspice_to_s1p.sh {} \;";
